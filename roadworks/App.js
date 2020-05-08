05ff8a133277b724b95fde32acad039f6003313b
Paulo Fernandes <palexandrefernandes@ipvc.pt>
2020-05-08 10:26:22 +0100

    Translations

--- roadworks/App.js
+++ roadworks/App.js
@@ -5,6 +5,7 @@ import Login from './src/screens/login/Login';
 import { AuthComponent } from './src/authentication/AuthProvider';
 import NotesRoute from './src/routes/NotesRoute';
 import MainRoute from './src/routes/MainRoute';
+import {TranslationProvider} from './src/translation/TranslationProvider';
 
 const Stack = createStackNavigator()
 
@@ -12,19 +13,21 @@ function App() {
   const [ state, setState ] = React.useState({})
 
   return (
-    <AuthComponent state={setState}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {!state.isLoggedIn ? (
-            <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-            ) : !state.token ? (
-              <Stack.Screen name="NotesRoute" component={NotesRoute} options={{headerShown: false}}/>
-            ) : (
-              <Stack.Screen name="MainRoute" component={MainRoute} options={{headerShown: false}}/>
-            )}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthComponent>
+    <TranslationProvider>
+      <AuthComponent state={setState}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            {!state.isLoggedIn ? (
+              <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+              ) : !state.token ? (
+                <Stack.Screen name="NotesRoute" component={NotesRoute} options={{headerShown: false}}/>
+              ) : (
+                <Stack.Screen name="MainRoute" component={MainRoute} options={{headerShown: false}}/>
+              )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthComponent>
+    </TranslationProvider>
     
   );
 }
